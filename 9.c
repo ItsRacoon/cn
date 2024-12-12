3dee65e749788ce67b82377b2852f9c0e3703eb6
Vishesh Kumar <154086150+ItsRacoon@users.noreply.github.com>
2024-12-13 00:27:52 +0530

    Update 9.c

--- 9.c
+++ 9.c
@@ -1,64 +1,53 @@
-#include<stdio.h>
-#include<strings.h>
-int min(int x,int y)
-{
-if(x<y)
-return x;
-else
-return y;
-}
-int main()
-{
-int drop=0,mini,nsec,cap,count=0,i,inp[25],process;
-system("clear");
-printf("Enter The Bucket Size\n");
-scanf("%d",&cap);
-printf("Enter The Operation Rate\n");
-scanf("%d",&process);
-printf("Enter The No. Of Seconds You Want To Stimulate\n");
-scanf("%d",&nsec);
-for(i=0;i<nsec;i++)
-{
-printf("Enter The Size Of The Packet Entering At %d sec\n",i+1);
-scanf("%d",&inp[i]);
-}
-printf("\nSecond|Packet
-Recieved|Packet
-Sent|Packet
-Left|Packet
-Dropped|\n");
-printf("-------------------------------------------------------------------------------\n");
-for(i=0;i<nsec;i++)
-{
-count+=inp[i];
-if(count>cap)
-{
-drop=count-cap;
-count=cap;
-}
-printf("%d",i+1);
-printf("\t%d",inp[i]);
-mini=min(count,process);
-printf("\t\t%d",mini);
-count=count-mini;
-printf("\t\t%d",count);
-printf("\t\t%d\n",drop);
-drop=0;
-}
-for(;count!=0;i++)
-{
-if(count>cap)
-{
-drop=count-cap;
-count=cap;
-}
-printf("%d",i+1);
-printf("\t0");
-mini=min(count,process);
-printf("\t\t%d",mini);
-count=count-mini;
-printf("\t\t%d",count);
-printf("\t\t%d\n",drop);
-}
+#include <stdio.h>
+#include <stdlib.h>  // For system() function
+#include <string.h>  // For string functions if needed
+
+int min(int x, int y) {
+    return (x < y) ? x : y;
 }
 
+int main() {
+    int drop = 0, mini, nsec, cap, count = 0, i, inp[25], process;
+    system("clear");  // Clears the terminal screen (might be system-dependent)
+
+    printf("Enter The Bucket Size: ");
+    scanf("%d", &cap);
+
+    printf("Enter The Operation Rate: ");
+    scanf("%d", &process);
+
+    printf("Enter The Number Of Seconds You Want To Simulate: ");
+    scanf("%d", &nsec);
+
+    for (i = 0; i < nsec; i++) {
+        printf("Enter The Size Of The Packet Entering At %d sec: ", i + 1);
+        scanf("%d", &inp[i]);
+    }
+
+    printf("\nSecond | Packet Received | Packet Sent | Packet Left | Packet Dropped\n");
+    printf("-----------------------------------------------------------------------\n");
+
+    for (i = 0; i < nsec; i++) {
+        count += inp[i];
+        if (count > cap) {
+            drop = count - cap;
+            count = cap;
+        }
+        printf("%6d | %15d | %11d | %11d | %14d\n", i + 1, inp[i], min(count, process), count - min(count, process), drop);
+        count -= min(count, process);
+        drop = 0;
+    }
+
+    while (count > 0) {
+        if (count > cap) {
+            drop = count - cap;
+            count = cap;
+        }
+        printf("%6d | %15d | %11d | %11d | %14d\n", i + 1, 0, min(count, process), count - min(count, process), drop);
+        count -= min(count, process);
+        i++;
+        drop = 0;
+    }
+
+    return 0;
+}
